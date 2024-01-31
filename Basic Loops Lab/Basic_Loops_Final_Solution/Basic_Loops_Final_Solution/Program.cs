777f8526ba622abb9d2387a8568a7d7cca092ffd
Zack Prohonic <zackprohonic20@gmail.com>
2024-01-30 22:32:58 -0500

    Basic Loops Lab

---
+++ Basic Loops Lab/Basic_Loops_Final_Solution/Basic_Loops_Final_Solution/Program.cs
@@ -0,0 +1,20 @@
+﻿int numberOfAttempts = 5;
+bool lockedDoor = true;
+do
+{
+    Console.WriteLine("Please enter passcode:");
+    int userInput = int.Parse(Console.ReadLine());
+    if (userInput == 13579)
+    {
+        Console.WriteLine("Welcome!");
+        lockedDoor = false;
+    }
+    else if (numberOfAttempts == 1)
+    {
+        Console.WriteLine("Too many incorrect attempts. Locked out.");
+        lockedDoor = false;  
+    }
+    numberOfAttempts--;
+
+} while (lockedDoor == true);
+
---
+++ Basic Loops Lab/Basic_Loops_Lab/Basic_Loops_Lab/Program.cs
@@ -0,0 +1,60 @@
+﻿bool loopAgain = false;
+do
+{
+    Console.WriteLine("Hello World!");
+    Console.WriteLine("Would you like to continue (y/n)?");
+    loopAgain = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+} while (loopAgain == true);
+
+Console.WriteLine("Goodbye!");
+
+bool playAgain = false;
+do
+{
+    Console.WriteLine("Please enter a number");
+    int userNumber = int.Parse(Console.ReadLine());
+    for (int i = userNumber; i >= 0; i--)
+    {
+        Console.WriteLine(i);
+    }
+    int secondLoop = 0;
+    do
+    {
+        Console.WriteLine(secondLoop);
+        secondLoop++;
+        
+    } while (secondLoop <= userNumber);
+    
+    Console.WriteLine("Would you like to continue? (y/n)");
+    playAgain = Console.ReadLine().ToLower().Trim() == "y" ? true : false;
+} while (playAgain == true);
+
+bool lockedDoor = true;
+int numberOfAttempts = 5;
+while (lockedDoor)
+{
+        Console.WriteLine("Please enter passcode: ");
+        int userInput = int.Parse(Console.ReadLine().Trim());
+        if (userInput == 13579)
+        {
+            Console.WriteLine("Welcome");
+            lockedDoor = false;
+        } 
+        else if (numberOfAttempts == 1)
+        {
+        Console.WriteLine("Too many incorrect attempts. System locked.");
+        lockedDoor = false;
+        }
+        numberOfAttempts--;
+
+} 
+
+
+
+
+
+
+
+
+
+
