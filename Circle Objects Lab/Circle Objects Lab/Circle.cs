61dbe668c0c1bda5bec6bd44d0a414d8c67704a9
Zack Prohonic <zackprohonic20@gmail.com>
2024-02-28 17:18:43 -0500

    Circle Objects Lab

---
+++ Circle Objects Lab/Circle Objects Lab/Circle.cs
@@ -0,0 +1,41 @@
+﻿
+
+namespace Circle_Objects_Lab
+{
+    public class Circle
+    {
+        private double Radius {  get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public static double CalculateDiameter(double radius)
+        {
+            return radius *2;
+        }
+
+        public static double CalculateCircumference(double radius)
+        {
+            return radius * 2 * double.Pi;
+        }
+
+        public static double CalculateArea(double radius)
+        {
+            return radius * radius * double.Pi;
+        }
+
+        public static void Grow(double radius)
+        {
+            radius = radius * 2;
+        }
+
+        public static double GetRadius(double radius)
+        {
+            return radius;
+        }
+    }
+
+
+}
---
+++ Circle Objects Lab/Circle Objects Lab/Program.cs
@@ -0,0 +1,38 @@
+﻿using Circle_Objects_Lab;
+
+double radius = 0;
+
+Console.WriteLine("Welcome to the Circle Generator");
+Console.WriteLine("Enter the radius of the circle: ");
+
+while (double.TryParse(Console.ReadLine(), out radius) == false)
+{
+    Console.WriteLine("Please enter a valid number");
+}
+
+Circle circle = new Circle(radius);
+
+while (true)
+{
+
+    Console.WriteLine($"Diameter: {Circle.CalculateDiameter(radius)}");
+    Console.WriteLine($"Circumference: {Circle.CalculateCircumference(radius)}");
+    Console.WriteLine($"Area: {Circle.CalculateArea(radius)}");
+
+    Console.WriteLine("Should the circle grow? (y/n)");
+    string userSelection = Console.ReadLine();
+
+    if (userSelection.ToLower() == "y")
+    {
+        Console.WriteLine("The circle is magically growing...");
+        Circle.Grow(radius);
+        radius = radius * 2;
+        //could not get Grow() method to properly double radius. attempted to save new radius value as double but received error stating void cannot be converted to double. Stumped me!
+    }
+    else
+    {
+        Console.WriteLine($"Goodbye. The circle's final radius is {Circle.GetRadius(radius)}");
+        break;
+    }
+
+}
